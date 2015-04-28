558d6f1d109d75e74ecf085ed34dad446a694282
低调的酱油党 <bsily1314lyl@gmail.com>
2015-04-28 17:13:17 +0800

    Revert "checkStyle the code"

--- email/src/main/java/com/vfixviii/mavenBook/account/email/exception/EmailException.java
+++ email/src/main/java/com/vfixviii/mavenBook/account/email/exception/EmailException.java
@@ -1,18 +1,8 @@
 package com.vfixviii.mavenBook.account.email.exception;
 
-/**
- * 邮件处理异常类.
- * <p/>
- * 抛出统一的邮件处理异常，方便进行各种xx
- */
-public class EmailException extends Exception {
+public class EmailException extends RuntimeException {
 
-    /**
-     * 邮件异常构造方法.
-     *
-     * @param s 异常消息
-     */
-    public EmailException(final String s) {
+    public EmailException(String s) {
         super(s);
     }
 }
