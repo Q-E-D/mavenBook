c0f344894b8a70c16b553f9fe82a7298521a8f13
Q-E-D <bsily1314lyl@gmail.com>
2015-05-07 18:14:44 +0800

    完善AccountCaptchaService实现类
    实现服务类方法，加入测试类

--- captcha/src/main/java/com/vfixviii/mavenBook/account/captcha/service/impl/AccountCaptchaServiceImpl.java
+++ captcha/src/main/java/com/vfixviii/mavenBook/account/captcha/service/impl/AccountCaptchaServiceImpl.java
@@ -1,19 +1,46 @@
 package com.vfixviii.mavenBook.account.captcha.service.impl;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
 import com.vfixviii.mavenBook.account.captcha.exception.AccountCaptchaException;
 import com.vfixviii.mavenBook.account.captcha.service.AccountCaptchaService;
+import com.vfixviii.mavenBook.account.captcha.util.RandomGeneratorUtil;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * 验证码图片生成实现类.
  */
-public class AccountCaptchaServiceImpl implements AccountCaptchaService {
+@Service
+public class AccountCaptchaServiceImpl implements AccountCaptchaService, InitializingBean {
 
+    private DefaultKaptcha kaptcha;
+
+    private List<String> preDefinedTexts;
+
+    private Map<String, String> captchaMap;
+
+    private Integer index = 0;
 
     @Override
     public String generateCaptchaKey() throws AccountCaptchaException {
-        return null;
+        String key = RandomGeneratorUtil.getRandomString(8);
+        String text = getGenerateText(key);
+        captchaMap.put(key, text);
+
+        return text;
+    }
+
+    private String getGenerateText(String key) {
+        if (preDefinedTexts != null && !preDefinedTexts.isEmpty()) {
+            return preDefinedTexts.get(index++ % preDefinedTexts.size());
+        } else {
+            return kaptcha.createText();
+        }
     }
 
     @Override
@@ -23,16 +50,25 @@ public class AccountCaptchaServiceImpl implements AccountCaptchaService {
 
     @Override
     public boolean valiateCaptcha(String key, String value) throws AccountCaptchaException {
+        if (key != null && value != null) {
+            return value.equals(captchaMap.get(key));
+        }
         return false;
     }
 
     @Override
     public List<String> getPreDefinedTexts() {
-        return null;
+        return this.preDefinedTexts;
     }
 
     @Override
     public void setPreDefinedTexts(List<String> preDefinedTexts) {
+        this.setPreDefinedTexts(preDefinedTexts);
+    }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.kaptcha = new DefaultKaptcha();
+        this.kaptcha.setConfig(new Config(new Properties()));
     }
 }
